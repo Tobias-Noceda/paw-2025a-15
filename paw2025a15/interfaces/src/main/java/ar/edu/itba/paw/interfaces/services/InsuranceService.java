872ed4893b603ac2eaef9486fba17c6d289f1a86
kndtxt <csilvadiniz@itba.edu.ar>
2025-04-02 19:54:21 -0300

    🚧insurance service in doctorController

--- paw2025a15/interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/InsuranceService.java
+++ paw2025a15/interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/InsuranceService.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.paw.interfaces.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import ar.edu.itba.paw.models.Insurance;
@@ -10,4 +11,6 @@ public interface InsuranceService {
     public void edit(long id, String name, long pictureId);
 
     public Optional<Insurance> getInsuranceById(long id);
+
+    public List<Insurance> getAllInsurances();
 }
