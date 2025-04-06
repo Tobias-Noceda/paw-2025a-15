97a47f6ecc1db219abb752b782c55fba765679d9
kndtxt <csilvadiniz@itba.edu.ar>
2025-04-06 16:12:45 -0300

    📄MonthEnum

---
+++ paw2025a15/models/src/main/java/ar/edu/itba/paw/models/MonthEnum.java
@@ -0,0 +1,34 @@
+package ar.edu.itba.paw.models;
+
+public enum MonthEnum {
+
+    JANUARY("January"),
+    FEBRUARY("February"),
+    MARCH("March"),
+    APRIL("April"),
+    JUNE("June"),
+    JULY("July"),
+    AUGUST("August"),
+    SEPTEMBER("September"),
+    OCTOBER("October"),
+    NOVEMBER("November"),
+    DECEMBER("December");
+
+    private final String name;
+
+    MonthEnum(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static MonthEnum fromInt(int num) {
+        MonthEnum[] values = MonthEnum.values();
+        if (num >= 0 && num < values.length) {
+            return values[num];
+        }
+        throw new IllegalArgumentException("Number out of range");
+    }
+}
