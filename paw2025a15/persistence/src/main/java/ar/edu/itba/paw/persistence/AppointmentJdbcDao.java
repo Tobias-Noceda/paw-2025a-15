5ad5a476f0fb08a65018e796d8be2923c1639a43
kndtxt <csilvadiniz@itba.edu.ar>
2025-04-12 19:43:26 -0300

    ✨Only future appointments ordered in profiles

--- paw2025a15/persistence/src/main/java/ar/edu/itba/paw/persistence/AppointmentJdbcDao.java
+++ paw2025a15/persistence/src/main/java/ar/edu/itba/paw/persistence/AppointmentJdbcDao.java
@@ -1,6 +1,8 @@
 package ar.edu.itba.paw.persistence;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -65,16 +67,16 @@ public class AppointmentJdbcDao implements AppointmentDao{
 
     @Override
     public List<AppointmentData> getAppointmentDataByPatientId(long patientId) {
-        return jdbcTemplate.query("SELECT ds.shift_id, u.user_name AS patient_name, d.user_name AS doctor_name, a.appointment_date, ds.shift_start_time, ds.shift_end_time, ds.shift_address FROM appointments AS a JOIN users AS u ON a.patient_id = u.user_id JOIN doctor_shifts AS ds ON ds.shift_id = a.shift_id JOIN users AS d ON ds.doctor_id = d.user_id WHERE a.patient_id = ?",
-          new Object[]  {patientId},
-          new int[] {java.sql.Types.BIGINT}, (rs, rowNum) -> new AppointmentData(rs.getLong("shift_id"), rs.getString("patient_name"), rs.getString("doctor_name"), rs.getDate("appointment_date").toLocalDate(), rs.getTime("shift_start_time").toLocalTime(), rs.getTime("shift_end_time").toLocalTime(), rs.getString("shift_address")));
+        return jdbcTemplate.query("SELECT ds.shift_id, u.user_name AS patient_name, d.user_name AS doctor_name, a.appointment_date, ds.shift_start_time, ds.shift_end_time, ds.shift_address FROM appointments AS a JOIN users AS u ON a.patient_id = u.user_id JOIN doctor_shifts AS ds ON ds.shift_id = a.shift_id JOIN users AS d ON ds.doctor_id = d.user_id WHERE a.patient_id = ? AND (a.appointment_date + ds.shift_start_time) > ? ORDER BY a.appointment_date ASC, ds.shift_start_time ASC",
+          new Object[]  {patientId, LocalDateTime.now()},
+          new int[] {java.sql.Types.BIGINT, java.sql.Types.TIMESTAMP}, (rs, rowNum) -> new AppointmentData(rs.getLong("shift_id"), rs.getString("patient_name"), rs.getString("doctor_name"), rs.getDate("appointment_date").toLocalDate(), rs.getTime("shift_start_time").toLocalTime(), rs.getTime("shift_end_time").toLocalTime(), rs.getString("shift_address")));
     }
 
     @Override
     public List<AppointmentData> getAppointmentDataByDoctorId(long doctorId) {
-        return jdbcTemplate.query("SELECT ds.shift_id, u.user_name AS patient_name, d.user_name AS doctor_name, a.appointment_date, ds.shift_start_time, ds.shift_end_time, ds.shift_address FROM appointments AS a JOIN users AS u ON a.patient_id = u.user_id JOIN doctor_shifts AS ds ON ds.shift_id = a.shift_id JOIN users AS d ON ds.doctor_id = d.user_id WHERE ds.doctor_id = ?",
-          new Object[]  {doctorId},
-          new int[] {java.sql.Types.BIGINT}, (rs, rowNum) -> new AppointmentData(rs.getLong("shift_id"), rs.getString("patient_name"), rs.getString("doctor_name"), rs.getDate("appointment_date").toLocalDate(), rs.getTime("shift_start_time").toLocalTime(), rs.getTime("shift_end_time").toLocalTime(), rs.getString("shift_address")));
+        return jdbcTemplate.query("SELECT ds.shift_id, u.user_name AS patient_name, d.user_name AS doctor_name, a.appointment_date, ds.shift_start_time, ds.shift_end_time, ds.shift_address FROM appointments AS a JOIN users AS u ON a.patient_id = u.user_id JOIN doctor_shifts AS ds ON ds.shift_id = a.shift_id JOIN users AS d ON ds.doctor_id = d.user_id WHERE ds.doctor_id = ? AND (a.appointment_date + ds.shift_start_time) > ? ORDER BY a.appointment_date ASC, ds.shift_start_time ASC",
+          new Object[]  {doctorId, LocalDateTime.now()},
+          new int[] {java.sql.Types.BIGINT, java.sql.Types.TIMESTAMP}, (rs, rowNum) -> new AppointmentData(rs.getLong("shift_id"), rs.getString("patient_name"), rs.getString("doctor_name"), rs.getDate("appointment_date").toLocalDate(), rs.getTime("shift_start_time").toLocalTime(), rs.getTime("shift_end_time").toLocalTime(), rs.getString("shift_address")));
     }
 
     @Override
