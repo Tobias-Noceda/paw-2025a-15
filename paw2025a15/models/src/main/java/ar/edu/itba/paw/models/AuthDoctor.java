71b435e34fbd26869fab813347eeebcc161c8aef
smesarubio <smesarubio@itba.edu.ar>
2025-05-18 22:47:52 -0300

    Implements User persistence with Hibernate
    Configures User model as a JPA entity.
    
    Creates a new UserHibernateDao class, implementing UserDao
    using Hibernate for database interactions.
    
    Adds methods for creating, retrieving, and updating user
    information in the database, like locale or password.
    Includes methods for paginated search of authorized patients.

--- paw2025a15/models/src/main/java/ar/edu/itba/paw/models/AuthDoctor.java
+++ paw2025a15/models/src/main/java/ar/edu/itba/paw/models/AuthDoctor.java
@@ -2,9 +2,18 @@ package ar.edu.itba.paw.models;
 
 import ar.edu.itba.paw.models.enums.AccessLevelEnum;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "auth_doctors")
 public class AuthDoctor {
+    @Column(name = "doctor_id")
     private final long doctorId;
+    @Column(name = "patient_id")
     private final long patientId;
+    @Column( name = "access_level")
     private final AccessLevelEnum accessLevel;
 
     public AuthDoctor(long doctorId, long patientId){
--- paw2025a15/persistence/src/main/java/ar/edu/itba/paw/persistence/UserHibernateDao.java
+++ paw2025a15/persistence/src/main/java/ar/edu/itba/paw/persistence/UserHibernateDao.java
@@ -4,13 +4,17 @@ import ar.edu.itba.paw.interfaces.persistence.UserDao;
 import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.models.enums.LocaleEnum;
 import ar.edu.itba.paw.models.enums.UserRoleEnum;
+import org.hibernate.Query;
 import org.hibernate.jpa.boot.spi.EntityManagerFactoryBuilder;
 import org.springframework.stereotype.Repository;
+import org.w3c.dom.css.CSSStyleDeclaration;
 //ver imports
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -27,6 +31,22 @@ public class UserHibernateDao implements UserDao {
     }
 
     @Override
+    public void editUser(long id, String name, String telephone, long pictureId) {
+        final TypedQuery<User>  query = em.createQuery("update User u set u.name = :name, u.telephone = :telephone where u.id = :id", User.class );
+        query.setParameter("name", name);
+        query.setParameter("telephone", telephone);
+        query.setParameter("id", id);
+        query.executeUpdate();
+    }
+
+    @Override
+    public void updateLocale(long userId, LocaleEnum locale) {
+        final TypedQuery<User> query = em.createQuery("update User u set u.locale = :locale", User.class );
+        query.setParameter("locale", locale);
+        query.executeUpdate();
+    }
+
+    @Override
     public Optional<User> getUserById(long id){
         return Optional.ofNullable(em.find(User.class, id));
     }
@@ -39,7 +59,50 @@ public class UserHibernateDao implements UserDao {
     }
 
     @Override
-    public void changePasswordByID(long id, String password){
-
+    public List<User> searchAuthPatientsPageByDoctorIdAndName(long doctorId, String name, int page, int pageSize) {
+        if(page <= 0 || pageSize <= 0) return Collections.emptyList();
+        int offset = (page - 1) * pageSize;
+        TypedQuery<User> query = em.createQuery(
+        "select distinct u from AuthDoctor as ad join User as u on ad.patientId = u.id where ad.doctorId = :doctorId ", User.class);
+        query.setParameter("doctorId", doctorId);
+        if(name != null && !name.trim().isEmpty()) {
+            query = em.createQuery("select distinct u from AuthDoctor as ad join User as u on ad.patientId = u.id where ad.doctorId = :doctorId AND LOWER(u.name) LIKE :userName ", User.class);
+            query.setParameter("doctorId", doctorId);
+            query.setParameter("userName","%" + sanitize(name) + "%");
+        }
+        query.setFirstResult(offset);
+        query.setMaxResults(pageSize);
+        return query.getResultList();
     }
+
+    @Override
+    public int searchAuthPatientsCountByDoctorIdAndName(long doctorId, String name) {
+        TypedQuery<Integer> query = em.createQuery(" select count(distinct ad.patientId) from AuthDoctor as ad join User as u on ad.patientId = u.id where ad.doctorId = :doctorId ", Integer.class);
+        query.setParameter("doctorId", doctorId);
+        if(name != null && !name.trim().isEmpty()) {
+            query = em.createQuery("from AuthDoctor as ad join User as u on ad.patientId = u.id where ad.doctorId = :doctorId AND LOWER(u.name) LIKE :userName", Integer.class);
+            query.setParameter("doctorId", doctorId);
+            query.setParameter("userName","%" + sanitize(name) + "%");
+        }
+        return query.getSingleResult();
+    }
+
+    @Override
+    public void changePasswordByID(long id, String password){
+        final TypedQuery<User> query = em.createQuery("update User u set u.password = :password where u.id = :id", User.class);
+        query.setParameter("password", password);
+        query.setParameter("id", id);
+        query.executeUpdate();
+    }
+
+    private String sanitize(String name) {
+        if (name == null) return null;
+        return name
+                .replace("\\", "\\\\\\")
+                .replace("%", "\\\\%")
+                .replace("_", "\\\\_")
+                .trim()
+                .toLowerCase();
+    }
+
 }
