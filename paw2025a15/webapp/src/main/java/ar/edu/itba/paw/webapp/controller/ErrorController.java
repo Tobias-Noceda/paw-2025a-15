3c5740f07ba5a9d2042d12770d43b236cf25dbc9
Tobias-Noceda <tnoceda@itba.edu.ar>
2025-05-31 17:58:56 -0300

    ➕: Recovered lost error controllers

---
+++ paw2025a15/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -0,0 +1,52 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ErrorController {
+    @RequestMapping("/400")
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ModelAndView badRequest() {
+        return new ModelAndView("errorPages/400");
+    }
+
+    @RequestMapping("/403")
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ModelAndView forbidden() {
+        return new ModelAndView("errorPages/403");
+    }
+
+    @RequestMapping("/404")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView notFound() {
+        return new ModelAndView("errorPages/404");
+    }
+
+    @RequestMapping("/405")
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ModelAndView notAllowed() {
+        return new ModelAndView("errorPages/405");
+    }
+
+    @RequestMapping("/408")
+    @ResponseStatus(HttpStatus.REQUEST_TIMEOUT)
+    public ModelAndView timeout() {
+        return new ModelAndView("errorPages/408");
+    }
+
+    @RequestMapping("/500")
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ModelAndView unhandledException() {
+        return new ModelAndView("errorPages/500");
+    }
+    
+    @RequestMapping("/{code:[0-9]{3}}")
+    public ModelAndView error(@PathVariable("code") String code) {
+        return new ModelAndView("errorPages/" + code);
+    }
+}
---
+++ paw2025a15/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorControllerAdvice.java
@@ -0,0 +1,79 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import java.net.http.HttpConnectTimeoutException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import ar.edu.itba.paw.models.exceptions.AlreadyExistsException;
+import ar.edu.itba.paw.models.exceptions.AppointmentAlreadyTakenException;
+import ar.edu.itba.paw.models.exceptions.FormErrorException;
+import ar.edu.itba.paw.models.exceptions.MediaTypeException;
+import ar.edu.itba.paw.models.exceptions.NotFoundException;
+import ar.edu.itba.paw.models.exceptions.UnauthorizedException;
+
+@ControllerAdvice
+public class ErrorControllerAdvice {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({
+        IllegalArgumentException.class,
+        AlreadyExistsException.class,
+        AppointmentAlreadyTakenException.class,
+        FormErrorException.class
+    })
+    public ModelAndView handle400Exception() {
+        ModelAndView mav = new ModelAndView("errorPages/400");
+        return mav;
+    }
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler({
+        AccessDeniedException.class
+    })
+    public ModelAndView handle403Exception() {
+        return new ModelAndView("403");
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({
+        NotFoundException.class,
+        NoHandlerFoundException.class,
+        UnauthorizedException.class
+    })
+    public ModelAndView handle404Exception() {
+        ModelAndView mav = new ModelAndView("errorPages/404");
+        return mav;
+    }
+
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ModelAndView handle405Exception() {
+        return new ModelAndView("errorPages/405");
+    }
+
+    @ResponseStatus(HttpStatus.REQUEST_TIMEOUT)
+    @ExceptionHandler(HttpConnectTimeoutException.class)
+    public ModelAndView handle408Exception() {
+        return new ModelAndView("errorPages/408");
+    }
+
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    @ExceptionHandler(MediaTypeException.class)
+    public ModelAndView handle415Exception() {
+        return new ModelAndView("errorPages/415");
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({
+        Exception.class,
+        RuntimeException.class
+    })
+    public ModelAndView handle500Exception() {
+        return new ModelAndView("errorPages/500");
+    }
+}
