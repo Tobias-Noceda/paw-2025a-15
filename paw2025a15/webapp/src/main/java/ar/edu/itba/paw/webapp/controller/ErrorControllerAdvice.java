79dd5aa1b003b17534481bd9d2e84dd13f551116
Tobias-Noceda <tnoceda@itba.edu.ar>
2025-06-04 19:02:48 -0300

    Enables error handling and fixes date issue
    Re-enables error handling by uncommenting the error controller and advice.
    
    Fixes a potential null pointer exception when calculating patient age
    if the patient's birthdate is null.

--- paw2025a15/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorControllerAdvice.java
+++ paw2025a15/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorControllerAdvice.java
@@ -20,60 +20,60 @@ import ar.edu.itba.paw.models.exceptions.UnauthorizedException;
 
 @ControllerAdvice
 public class ErrorControllerAdvice {
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler({
-//        IllegalArgumentException.class,
-//        AlreadyExistsException.class,
-//        AppointmentAlreadyTakenException.class,
-//        FormErrorException.class
-//    })
-//    public ModelAndView handle400Exception() {
-//        ModelAndView mav = new ModelAndView("errorPages/400");
-//        return mav;
-//    }
-//    @ResponseStatus(HttpStatus.FORBIDDEN)
-//    @ExceptionHandler({
-//        AccessDeniedException.class
-//    })
-//    public ModelAndView handle403Exception() {
-//        return new ModelAndView("403");
-//    }
-//
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler({
-//        NotFoundException.class,
-//        NoHandlerFoundException.class,
-//        UnauthorizedException.class
-//    })
-//    public ModelAndView handle404Exception() {
-//        ModelAndView mav = new ModelAndView("errorPages/404");
-//        return mav;
-//    }
-//
-//    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-//    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-//    public ModelAndView handle405Exception() {
-//        return new ModelAndView("errorPages/405");
-//    }
-//
-//    @ResponseStatus(HttpStatus.REQUEST_TIMEOUT)
-//    @ExceptionHandler(HttpConnectTimeoutException.class)
-//    public ModelAndView handle408Exception() {
-//        return new ModelAndView("errorPages/408");
-//    }
-//
-//    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-//    @ExceptionHandler(MediaTypeException.class)
-//    public ModelAndView handle415Exception() {
-//        return new ModelAndView("errorPages/415");
-//    }
-//
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    @ExceptionHandler({
-//        Exception.class,
-//        RuntimeException.class
-//    })
-//    public ModelAndView handle500Exception() {
-//        return new ModelAndView("errorPages/500");
-//    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({
+        IllegalArgumentException.class,
+        AlreadyExistsException.class,
+        AppointmentAlreadyTakenException.class,
+        FormErrorException.class
+    })
+    public ModelAndView handle400Exception() {
+        ModelAndView mav = new ModelAndView("errorPages/400");
+        return mav;
+    }
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler({
+        AccessDeniedException.class
+    })
+    public ModelAndView handle403Exception() {
+        return new ModelAndView("403");
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({
+        NotFoundException.class,
+        NoHandlerFoundException.class,
+        UnauthorizedException.class
+    })
+    public ModelAndView handle404Exception() {
+        ModelAndView mav = new ModelAndView("errorPages/404");
+        return mav;
+    }
+
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ModelAndView handle405Exception() {
+        return new ModelAndView("errorPages/405");
+    }
+
+    @ResponseStatus(HttpStatus.REQUEST_TIMEOUT)
+    @ExceptionHandler(HttpConnectTimeoutException.class)
+    public ModelAndView handle408Exception() {
+        return new ModelAndView("errorPages/408");
+    }
+
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    @ExceptionHandler(MediaTypeException.class)
+    public ModelAndView handle415Exception() {
+        return new ModelAndView("errorPages/415");
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({
+        Exception.class,
+        RuntimeException.class
+    })
+    public ModelAndView handle500Exception() {
+        return new ModelAndView("errorPages/500");
+    }
 }
