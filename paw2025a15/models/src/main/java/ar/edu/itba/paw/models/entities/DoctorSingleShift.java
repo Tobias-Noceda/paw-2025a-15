a90dc1dae806d2c6dd345e3da59d45b4c2c39de1
knd.txt <csilvadiniz@itba.edu.ar>
2025-06-17 19:19:07 -0300

    🐛correct sequence in shifts

--- paw2025a15/models/src/main/java/ar/edu/itba/paw/models/entities/DoctorSingleShift.java
+++ paw2025a15/models/src/main/java/ar/edu/itba/paw/models/entities/DoctorSingleShift.java
@@ -31,8 +31,8 @@ import ar.edu.itba.paw.models.enums.WeekdayEnum;
 )
 public class DoctorSingleShift {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "doctor_shifts_shift_id_seq")
-    @SequenceGenerator(sequenceName = "doctor_shifts_shift_id_seq", name = "doctor_shifts_shift_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "doctor_single_shifts_shift_id_seq")
+    @SequenceGenerator(sequenceName = "doctor_single_shifts_shift_id_seq", name = "doctor_shifts_shift_id_seq", allocationSize = 1)
     @Column(name = "shift_id")
     private Long id;
 
