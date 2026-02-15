ad6ca6f053d3ecd71cfb445f064d254a4d797012
knd.txt <csilvadiniz@itba.edu.ar>
2026-02-15 13:41:51 -0300

    🖼️mediatypes

--- models/src/main/java/ar/edu/itba/paw/models/enums/AppointmentStatusEnum.java
+++ models/src/main/java/ar/edu/itba/paw/models/enums/AppointmentStatusEnum.java
@@ -1,9 +1,9 @@
 package ar.edu.itba.paw.models.enums;
 
 public enum AppointmentStatusEnum {
-    FREE("Free"),
-    TAKEN("Taken"),
-    COMPLETED("Completed");
+    FREE("free"),
+    TAKEN("taken"),
+    COMPLETED("completed");
 
     private final String displayName;
 
@@ -15,7 +15,7 @@ public enum AppointmentStatusEnum {
         return displayName;
     }
 
-    public static AppointmentStatusEnum fromString(String status) {
+    public static AppointmentStatusEnum fromString(String status) {//TODO sacar tolower de los otros casos y otros enum
         for (AppointmentStatusEnum s : AppointmentStatusEnum.values()) {
             if (s.displayName.equalsIgnoreCase(status)) {
                 return s;
