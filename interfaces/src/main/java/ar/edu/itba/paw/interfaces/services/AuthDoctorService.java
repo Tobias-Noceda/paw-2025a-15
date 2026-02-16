4ba1a9a970cf4769359e7fc1029542ffeca2ce5a
knd.txt <csilvadiniz@itba.edu.ar>
2026-02-16 08:18:45 -0300

    🔥deprecated

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/AuthDoctorService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/AuthDoctorService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import ar.edu.itba.paw.models.enums.AccessLevelEnum;
 
-public interface AuthDoctorService {//TODO check deprecated
+public interface AuthDoctorService {
     
     public void toggleAuthDoctor(long patientId, long doctorId);
 
@@ -13,7 +13,4 @@ public interface AuthDoctorService {//TODO check deprecated
     public boolean hasAuthDoctor(long patientId, long doctorId);
 
     public List<AccessLevelEnum> getAuthAccessLevelEnums(long patientId, long doctorId);
-    
-    @Deprecated
-    public void deauthorizeAllDoctors(long patientId);
 }
--- persistence/src/main/java/ar/edu/itba/paw/persistence/AuthDoctorJpaDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/AuthDoctorJpaDao.java
@@ -103,11 +103,4 @@ public class AuthDoctorJpaDao implements AuthDoctorDao{
                             .setParameter("patientId", patientId)
                             .getResultList();
     }
-
-    @Override
-    public void deauthorizeAllDoctors(long patientId) {
-        em.createQuery("DELETE FROM AuthDoctor ad WHERE ad.patient.id = :patientId")
-            .setParameter("patientId", patientId)
-            .executeUpdate();
-    }
 }
