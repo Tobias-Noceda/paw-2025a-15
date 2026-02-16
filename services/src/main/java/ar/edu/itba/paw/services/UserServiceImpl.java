a9b110c1ea7717e2b0b99213507a24caaa900e7b
Tobias-Noceda <tnoceda@itba.edu.ar>
2026-02-16 01:47:19 -0300

    🚧[WIP]: Created auto deletion method for unverified users

--- services/src/main/java/ar/edu/itba/paw/services/UserServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/UserServiceImpl.java
@@ -1,19 +1,25 @@
 package ar.edu.itba.paw.services;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ar.edu.itba.paw.interfaces.persistence.UserDao;
+import ar.edu.itba.paw.interfaces.services.DoctorService;
 import ar.edu.itba.paw.interfaces.services.EmailService;
+import ar.edu.itba.paw.interfaces.services.PatientService;
 import ar.edu.itba.paw.interfaces.services.UserService;
 import ar.edu.itba.paw.models.entities.User;
+import ar.edu.itba.paw.models.enums.UserRoleEnum;
 import ar.edu.itba.paw.models.exceptions.NotFoundException;
 
 @Service
@@ -26,6 +32,12 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private EmailService es;
+
+    @Autowired
+    private PatientService ps;
+
+    @Autowired
+    private DoctorService ds;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -56,5 +68,29 @@ public class UserServiceImpl implements UserService {
         User user = getUserByEmail(email).orElseThrow(() -> new NotFoundException("User with email: " + email + " does not exist!"));
         es.sendPasswordResetEmail(user, UUID.randomUUID().toString());
         LOGGER.info("Password recovery requested for user with email: {}", email);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public void verifyUser(String email) {
+        getUserByEmail(email).orElseThrow(() -> new NotFoundException("User with email: " + email + " does not exist!"));
+        userDao.verifyUser(email);
+        LOGGER.info("Verified user with email: {}", email);
+    }
+
+    @Transactional(readOnly = true)
+    @Scheduled(cron = "0 0 0 * * ?", zone = "America/Argentina/Buenos_Aires")
+    public void deleteUnverifiedUsers() {
+        List<User> unverifiedUsers = userDao.getUnverifiedUsersOlderThanDays(2);
+        
+        for (User user : unverifiedUsers) {
+            if (user.getRole().equals(UserRoleEnum.PATIENT)) {
+                ps.deletePatientById(user.getId());
+            } else if (user.getRole().equals(UserRoleEnum.DOCTOR)) {
+                ds.deleteDoctor(user.getId());
+            }
+            LOGGER.info("Deleted unverified user with email: {}", user.getEmail());
+        }
+        LOGGER.info("Tomorrow appointments reminder sent. At " + LocalDateTime.now().toLocalTime());
     }
 }
